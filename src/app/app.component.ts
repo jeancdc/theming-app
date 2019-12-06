e736d98804a23074faebfb5302fc2387b9ba340b
Jean Creuzé des Châtelliers <jean.creuzedeschatelliers@sylpheo.com>
2019-12-06 17:10:10 +0100

    some examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'theming-app';
+  email = new FormControl('', [Validators.required, Validators.email]);
+  myFormGroup: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.myFormGroup = fb.group({});
+  }
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+      this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {
+  MatFormFieldModule,
+  MatNativeDateModule,
+  MatButtonModule,
+  MatDividerModule,
+  MatListModule,
+  MatDatepickerModule,
+  MatInputModule,
+  MatIconModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatProgressSpinnerModule,
+  MatSliderModule,
+  MatRadioModule,
+  MatTabsModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +28,26 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatSliderModule,
+    MatRadioModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
